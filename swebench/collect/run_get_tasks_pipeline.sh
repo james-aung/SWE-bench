b7f504091d2e47eae10bf20be8ad2dfc396f77f9
James Aung <james.aung@c-openai.com>
2024-08-19 13:42:17 +0100

    scrape original 12 repos

--- swebench/collect/run_get_tasks_pipeline.sh
+++ swebench/collect/run_get_tasks_pipeline.sh
@@ -5,7 +5,10 @@
 # * Declare GITHUB_TOKENS=token1,token2,token3...
 
 python get_tasks_pipeline.py \
-    --repos 'scikit-learn/scikit-learn' \
+    --repos 'astropy/astropy', 'django/django', 'matplotlib/matplotlib', \
+            'mwaskom/seaborn', 'pallets/flask', 'psf/requests', \
+            'pydata/xarray', 'pylint-dev/pylint', 'pytest-dev/pytest', \
+            'scikit-learn/scikit-learn', 'sphinx-doc/sphinx', 'sympy/sympy' \
     --path_prs 'scrapes/20231101/prs' \
     --path_tasks 'scrapes/20231101/tasks' \
     --cutoff_date '20231101'
