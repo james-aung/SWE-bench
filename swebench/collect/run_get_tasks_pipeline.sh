fd100c78a098ec150281480f4d5129ced70a65aa
James Aung <james.aung@c-openai.com>
2024-08-26 15:54:01 +0000

    setup scrape for all repos from 202407

--- swebench/collect/run_get_tasks_pipeline.sh
+++ swebench/collect/run_get_tasks_pipeline.sh
@@ -4,7 +4,12 @@
 # * Create a .env file in this folder
 # * Declare GITHUB_TOKENS=token1,token2,token3...
 
+# Read the repos from the file and join them with a comma
+REPOS=$(cat ../../repos.txt)
+
+# Run the Python script with the repos
 python get_tasks_pipeline.py \
-    --repos 'scikit-learn/scikit-learn', 'pallets/flask' \
-    --path_prs '<path to folder to save PRs to>' \
-    --path_tasks '<path to folder to save tasks to>'
+    --repos $REPOS \
+    --path_prs 'scrapes/20240701/prs' \
+    --path_tasks 'scrapes/20240701/tasks' \
+    --cutoff_date '20240701'    
